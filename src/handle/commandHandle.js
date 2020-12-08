54b9d9a20fd9fe36df5c8b954c609e8711e75614
SadBoy <69816891+anmeeeeee@users.noreply.github.com>
2020-12-08 14:04:25 +0700

    Create commandHandle.js

---
+++ src/handle/commandHandle.js
@@ -0,0 +1,36 @@
+const { readdirSync } = require("fs");
+const ascii = require("ascii-table");
+
+let table = new ascii("Commands");
+table.setHeading("Command", "Load status");
+
+module.exports = client => {
+  readdirSync("./commands/").forEach(dir => {
+
+    const commands = readdirSync(`./commands/${dir}/`).filter(file =>
+      file.endsWith(".js")
+    );
+
+    for (let file of commands) {
+      let pull = require(`../commands/${dir}/${file}`);
+
+      if (pull.name) {
+        client.commands.set(pull.name, pull);
+
+        table.addRow(file, "✅");
+      } else {
+        table.addRow(
+          file,
+          `❌`
+        );
+
+        continue;
+      }
+
+      if (pull.aliases && Array.isArray(pull.alias))
+        pull.alias.forEach(aliases => client.alias.set(aliases, pull.name));
+    }
+  });
+
+  console.log(table.toString())
+};
